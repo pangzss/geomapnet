d314107646a592dde5c29e5088f85acf42948abd
pangzss <shawnpzs@hotmail.com>
2020-04-30 22:16:16 +0200

    local:04302216

--- contextual_loss/CX_distance.py
+++ contextual_loss/CX_distance.py
@@ -25,11 +25,11 @@ def CX_sim(x, y, h=0.5):
     cosine_sim = torch.bmm(x_normalized.transpose(1, 2), y_normalized)           # (N, H*W, H*W)
     
     d = (1 - cosine_sim)/2                                 # (N, H*W, H*W)  d[n, i, j] means d_ij for n-th data 
-    d_min, _ = torch.min(d, dim=2, keepdim=True)        # (N, H*W, 1)
-
+    d_min, idces = torch.min(d, dim=2, keepdim=True)        # (N, H*W, 1)
+    
     # Eq (2)
     d_tilde = d / (d_min + 1e-5)
-  
+    
     # Eq(3)
     w = torch.exp((1 - d_tilde) / h)
 
@@ -52,7 +52,7 @@ def CX_sim_NNDR(x, y, h=0.5):
   """
   assert x.size() == y.size()
   N, C, H, W = x.size()   # e.g., 10 x 512 x 14 x 14. In this case, the number of points is 196 (14x14).
-  
+
   y_mu = y.mean(3).mean(2).mean(0).reshape(1, -1, 1, 1)
   x_centered = x - y_mu
   y_centered = y - y_mu
@@ -66,33 +66,34 @@ def CX_sim_NNDR(x, y, h=0.5):
   cosine_sim = torch.bmm(x_normalized.transpose(1, 2), y_normalized)           # (N, H*W, H*W)
   
   d = (1 - cosine_sim)/2                                 # (N, H*W, H*W)  d[n, i, j] means d_ij for n-th data 
+  '''
+  d_min = torch.zeros_like(d)
+  for i in range(H*W):
+    mask = torch.zeros_like(d,dtype=bool)
+    mask[:,:,i] = True
+    d_i = d[~mask].reshape(N,H*W,H*W-1)
+    d_min[:,:,i],_ = torch.min(d_i,dim=2)
+  '''
+  d_min, idces = torch.min(d, dim=2, keepdim=True)        # (N, H*W, 1)
+  d_min = torch.repeat_interleave(d_min,H*W,dim=2)
   d_2nd_min = torch.topk(d,k=2,dim=2,largest=False)[0][:,:,1]
-  d_2nd_min = d_2nd_min[:,:,None]
-  
-  w = d_2nd_min / (d + 1e-5)
+  mask = torch.zeros_like(d,dtype=bool)
+  mask.scatter_(2,idces,True)
+  d_min[mask] = d_2nd_min.flatten()
 
+  d_tilde = d / (d_min+1e-5)
+
+  w = torch.exp((1-d_tilde)/h)
   # Eq(4)
-  cx_ij = torch.nn.functional.softmax(w,dim=2)
-
-
+  cx_ij = w / torch.sum(w,dim=2,keepdim=True)
   # Eq (1)
-  cx = torch.mean(torch.max(cx_ij, dim=1)[0], dim=1)  # (N, )
+  cx = torch.mean(torch.diagonal(cx_ij, offset=0,dim1=1,dim2=2), dim=1)  # (N, )
   return cx
 
 if __name__ == '__main__':
-    seed = 0
-    a = torch.tensor([4,20,60,70])
+   a = torch.randn(5,2,5,5)
+   b = a
 
-    topk = torch.topk(a,k=2,largest=False)[0]
-    # cx 
-    a_min = topk[0]
-    a_tilde = a/(a_min+1e-5)
-    w1 = torch.exp((1 - a_tilde) / 0.5)
-    w1 = w1/torch.sum(w1)
-    
-    # NNDR
-    a_2nd_min = topk[1]
-    w2 = a_2nd_min / (a + 1e-5)
-    w2 = torch.nn.functional.softmax(w2)
-
+   print(CX_sim(a,b))
+   print(CX_sim_NNDR(a,b))
     
